819927007a6b5d7abd1581b65489581ec1bdc19a
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-02-15 14:43:09 +0000

    Added the add new task option

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,29 @@
 class TasksController < ApplicationController
-
-   def index
+  def index
     @tasks = Task.all
   end
 
   def show
+    @task = find_task
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(set_task)
+    # @task = Task.new(params[task])
+    @task.save
+  end
+
+  private
+
+  def find_task
     @task = Task.find(params[:id])
+  end
+# Setting strong params below
+  def set_task
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Creating route to list tasks
   get '/tasks', to: 'tasks#index'
+  # Creating route to display task form
+  get '/tasks/new', to: 'tasks#new'
+  # Creating route to handle POST request generated when submitting form
+  post '/tasks', to: 'tasks#create'
   # Creating route to display details of task
   get '/tasks/:id', to: 'tasks#show', as: :task
-  # Creating route to display task form
-  # Creating route to handle POST request generated when submitting form
 end
