96e7fb05177af29d74a5e97a867a96b8ef1236fa
Ahmed Warsama <ahmedwarsama@live.co.uk>
2022-02-15 13:36:54 +0000

    Created routes for listing tasks and displaying details of tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Creating route to list tasks
+  get '/tasks', to: 'tasks#index'
+  # Creating route to display details of task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # Creating route to display task form
+  # Creating route to handle POST request generated when submitting form
 end
